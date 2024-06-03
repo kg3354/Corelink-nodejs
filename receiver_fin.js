1dd1e02f55931041f89a0197d2f0fe1f14342883
Kaiwen Guo <kg3354@nyu.edu>
2024-06-03 07:25:25 -0700

    removed buffer rotation

--- receiver_fin.js
+++ receiver_fin.js
@@ -55,89 +55,89 @@
 // // // };
 
 // // // run();
-// // const corelink = require('corelink-client');
-// // const { spawn } = require('child_process');
-// // const config = {
-// //   ControlPort: 20012,
-// //   ControlIP: 'corelink.hpc.nyu.edu',
-// //   autoReconnect: false,
-// // };
-// // const username = 'Testuser';
-// // const password = 'Testpassword';
-// // const workspace = 'Fenton';
-// // const protocol = 'ws';
-// // const datatype = 'distance';
+const corelink = require('corelink-client');
+const { spawn } = require('child_process');
+const config = {
+  ControlPort: 20012,
+  ControlIP: 'corelink.hpc.nyu.edu',
+  autoReconnect: false,
+};
+const username = 'Testuser';
+const password = 'Testpassword';
+const workspace = 'Fenton';
+const protocol = 'ws';
+const datatype = 'distance';
 
-// // const fileParts = {};
-// // let allFrames = [];
-// // counter = 0;
+const fileParts = {};
+let allFrames = [];
+counter = 0;
 
-// // const run = async () => {
-// //   if (await corelink.connect({ username, password }, config).catch((err) => { console.log(err) })) {
-// //     const receiver = await corelink.createReceiver({
-// //       workspace,
-// //       protocol,
-// //       type: datatype,
-// //       echo: true,
-// //       alert: true,
-// //     }).catch((err) => { console.log(err) });
+const run = async () => {
+  if (await corelink.connect({ username, password }, config).catch((err) => { console.log(err) })) {
+    const receiver = await corelink.createReceiver({
+      workspace,
+      protocol,
+      type: datatype,
+      echo: true,
+      alert: true,
+    }).catch((err) => { console.log(err) });
 
-// //     corelink.on('receiver', async (data) => {
-// //       const options = { streamIDs: [data.streamID] };
-// //       await corelink.subscribe(options);
-// //       console.log('Receiver and sender connected, subscribing to data.');
-// //     });
+    corelink.on('receiver', async (data) => {
+      const options = { streamIDs: [data.streamID] };
+      await corelink.subscribe(options);
+      console.log('Receiver and sender connected, subscribing to data.');
+    });
 
-// //     corelink.on('data', (streamID, data) => {
-// //       if (data.toString() === 'FINISHED') {
-// //         console.log('Received FINISHED marker.');
-// //         sendFramesToPython(allFrames);
-// //         allFrames = [];
-// //       } else {
-// //         const frameNumber = data[0];
-// //         const sliceIndex = data[1];
-// //         const totalSlices = data[2];
-// //         const content = data.slice(3);
-// //         console.log(`Frame number ${frameNumber} and slice number ${sliceIndex}`)
-// //         if (!fileParts[frameNumber]) {
-// //           fileParts[frameNumber] = new Array(totalSlices).fill(null);
-// //         }
+    corelink.on('data', (streamID, data) => {
+      if (data.toString() === 'FINISHED') {
+        console.log('Received FINISHED marker.');
+        sendFramesToPython(allFrames);
+        allFrames = [];
+      } else {
+        const frameNumber = data[0];
+        const sliceIndex = data[1];
+        const totalSlices = data[2];
+        const content = data.slice(3);
+        console.log(`Frame number ${frameNumber} and slice number ${sliceIndex}`)
+        if (!fileParts[frameNumber]) {
+          fileParts[frameNumber] = new Array(totalSlices).fill(null);
+        }
 
-// //         fileParts[frameNumber][sliceIndex] = content;
+        fileParts[frameNumber][sliceIndex] = content;
 
-// //         // Check if all parts are received
-// //         if (fileParts[frameNumber].every(part => part !== null)) {
-// //           const fullFile = Buffer.concat(fileParts[frameNumber]);
-// //           console.log(`Frame ${frameNumber} reassembled.`);
+        // Check if all parts are received
+        if (fileParts[frameNumber].every(part => part !== null)) {
+          const fullFile = Buffer.concat(fileParts[frameNumber]);
+          console.log(`Frame ${frameNumber} reassembled.`);
 
-// //           // Store the reassembled frame
-// //           allFrames.push(fullFile);
-// //         }
-// //       }
-// //     });
-// //   }
-// // };
+          // Store the reassembled frame
+          allFrames.push(fullFile);
+        }
+      }
+    });
+  }
+};
 
-// // const sendFramesToPython = (frames) => {
-// //   const pythonProcess = spawn('python', ['image_process.py']);
+const sendFramesToPython = (frames) => {
+  const pythonProcess = spawn('python', ['image_process.py']);
 
-// //   pythonProcess.stdin.write(Buffer.concat(frames));
-// //   pythonProcess.stdin.end();
+  pythonProcess.stdin.write(Buffer.concat(frames));
+  pythonProcess.stdin.end();
 
-// //   pythonProcess.stdout.on('data', (data) => {
-// //     console.log(`Python output: ${data}`);
-// //   });
+  pythonProcess.stdout.on('data', (data) => {
+    console.log(`Python output: ${data}`);
+  });
 
-// //   pythonProcess.stderr.on('data', (data) => {
-// //     console.error(`Python error: ${data}`);
-// //   });
+  pythonProcess.stderr.on('data', (data) => {
+    console.error(`Python error: ${data}`);
+  });
 
-// //   pythonProcess.on('close', (code) => {
-// //     console.log(`Python script finished with code ${code}`);
-// //   });
-// // };
+  pythonProcess.on('close', (code) => {
+    console.log(`Python script finished with code ${code}`);
+  });
+};
 
-// // run();
+run();
 // const corelink = require('corelink-client');
 // const { spawn } = require('child_process');
 // const config = {
@@ -232,97 +232,97 @@
 // };
 
 // run();
-const corelink = require('corelink-client');
-const { spawn } = require('child_process');
-const config = {
-  ControlPort: 20012,
-  ControlIP: 'corelink.hpc.nyu.edu',
-  autoReconnect: false,
-};
-const username = 'Testuser';
-const password = 'Testpassword';
-const workspace = 'Fenton';
-const protocol = 'ws';
-const datatype = 'distance';
+// const corelink = require('corelink-client');
+// const { spawn } = require('child_process');
+// const config = {
+//   ControlPort: 20012,
+//   ControlIP: 'corelink.hpc.nyu.edu',
+//   autoReconnect: false,
+// };
+// const username = 'Testuser';
+// const password = 'Testpassword';
+// const workspace = 'Fenton';
+// const protocol = 'ws';
+// const datatype = 'distance';
 
-const fileParts = {};
-let buffers = [[], []];
-let currentBufferIndex = 0;
-let frameCounter = 0;
-const maxFramesPerBuffer = 150;
+// const fileParts = {};
+// let buffers = [[], []];
+// let currentBufferIndex = 0;
+// let frameCounter = 0;
+// const maxFramesPerBuffer = 150;
 
-const run = async () => {
-  if (await corelink.connect({ username, password }, config).catch((err) => { console.log(err) })) {
-    const receiver = await corelink.createReceiver({
-      workspace,
-      protocol,
-      type: datatype,
-      echo: true,
-      alert: true,
-    }).catch((err) => { console.log(err) });
+// const run = async () => {
+//   if (await corelink.connect({ username, password }, config).catch((err) => { console.log(err) })) {
+//     const receiver = await corelink.createReceiver({
+//       workspace,
+//       protocol,
+//       type: datatype,
+//       echo: true,
+//       alert: true,
+//     }).catch((err) => { console.log(err) });
 
-    corelink.on('receiver', async (data) => {
-      const options = { streamIDs: [data.streamID] };
-      await corelink.subscribe(options);
-      console.log('Receiver and sender connected, subscribing to data.');
-    });
+//     corelink.on('receiver', async (data) => {
+//       const options = { streamIDs: [data.streamID] };
+//       await corelink.subscribe(options);
+//       console.log('Receiver and sender connected, subscribing to data.');
+//     });
 
-    corelink.on('data', (streamID, data) => {
-      if (data.toString() === 'FINISHED') {
-        console.log('Received FINISHED marker.');
-        sendFramesToPython(buffers[currentBufferIndex]);
-        buffers[currentBufferIndex] = [];  // Clear the current buffer
-        currentBufferIndex = 1 - currentBufferIndex;  // Switch buffer
-      } else {
-        const frameNumber = data.readUInt32BE(0); // Read 4 bytes for the frame number
-        const sliceIndex = data[4];
-        const totalSlices = data[5];
-        const content = data.slice(6);
-        console.log(`Frame number ${frameNumber} and slice number ${sliceIndex}`);
-        if (!fileParts[frameNumber]) {
-          fileParts[frameNumber] = new Array(totalSlices).fill(null);
-        }
+//     corelink.on('data', (streamID, data) => {
+//       if (data.toString() === 'FINISHED') {
+//         console.log('Received FINISHED marker.');
+//         sendFramesToPython(buffers[currentBufferIndex]);
+//         buffers[currentBufferIndex] = [];  // Clear the current buffer
+//         currentBufferIndex = 1 - currentBufferIndex;  // Switch buffer
+//       } else {
+//         const frameNumber = data.readUInt32BE(0); // Read 4 bytes for the frame number
+//         const sliceIndex = data[4];
+//         const totalSlices = data[5];
+//         const content = data.slice(6);
+//         console.log(`Frame number ${frameNumber} and slice number ${sliceIndex}`);
+//         if (!fileParts[frameNumber]) {
+//           fileParts[frameNumber] = new Array(totalSlices).fill(null);
+//         }
 
-        fileParts[frameNumber][sliceIndex] = content;
+//         fileParts[frameNumber][sliceIndex] = content;
 
-        // Check if all parts are received
-        if (fileParts[frameNumber].every(part => part !== null)) {
-          const fullFile = Buffer.concat(fileParts[frameNumber]);
-          console.log(`Frame ${frameNumber} reassembled.`);
+//         // Check if all parts are received
+//         if (fileParts[frameNumber].every(part => part !== null)) {
+//           const fullFile = Buffer.concat(fileParts[frameNumber]);
+//           console.log(`Frame ${frameNumber} reassembled.`);
 
-          // Store the reassembled frame
-          buffers[currentBufferIndex].push(fullFile);
-          frameCounter++;
+//           // Store the reassembled frame
+//           buffers[currentBufferIndex].push(fullFile);
+//           frameCounter++;
 
-          if (frameCounter >= maxFramesPerBuffer) {
-            sendFramesToPython(buffers[currentBufferIndex]);
-            buffers[currentBufferIndex] = [];  // Clear the current buffer
-            currentBufferIndex = 1 - currentBufferIndex;  // Switch buffer
-            frameCounter = 0;
-          }
-        }
-      }
-    });
-  }
-};
+//           if (frameCounter >= maxFramesPerBuffer) {
+//             sendFramesToPython(buffers[currentBufferIndex]);
+//             buffers[currentBufferIndex] = [];  // Clear the current buffer
+//             currentBufferIndex = 1 - currentBufferIndex;  // Switch buffer
+//             frameCounter = 0;
+//           }
+//         }
+//       }
+//     });
+//   }
+// };
 
-const sendFramesToPython = (frames) => {
-  const pythonProcess = spawn('python', ['image_process.py']);
+// const sendFramesToPython = (frames) => {
+//   const pythonProcess = spawn('python', ['image_process.py']);
 
-  pythonProcess.stdin.write(Buffer.concat(frames));
-  pythonProcess.stdin.end();
+//   pythonProcess.stdin.write(Buffer.concat(frames));
+//   pythonProcess.stdin.end();
 
-  pythonProcess.stdout.on('data', (data) => {
-    console.log(`Python output: ${data}`);
-  });
+//   pythonProcess.stdout.on('data', (data) => {
+//     console.log(`Python output: ${data}`);
+//   });
 
-  pythonProcess.stderr.on('data', (data) => {
-    console.error(`Python error: ${data}`);
-  });
+//   pythonProcess.stderr.on('data', (data) => {
+//     console.error(`Python error: ${data}`);
+//   });
 
-  pythonProcess.on('close', (code) => {
-    console.log(`Python script finished with code ${code}`);
-  });
-};
+//   pythonProcess.on('close', (code) => {
+//     console.log(`Python script finished with code ${code}`);
+//   });
+// };
 
-run();
+// run();
